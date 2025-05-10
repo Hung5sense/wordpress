830d1339aff277c0c0401e83dd05aab7d6eb17e5
HungDoan <doananhlehung@gmail.com>
2025-05-10 20:46:11 +0700

    update source

---
+++ wordpress_data/wp-content/themes/taildwindtheme/functions.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Tailwind Theme functions and definitions.
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Tailwind_Theme
+ * @subpackage Tailwind_Theme
+ * @since Tailwind Theme 1.0
+ */
---
+++ wordpress_data/wp-content/themes/taildwindtheme/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php bloginfo('name'); ?></title>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+</head>
+<body>
+    <h1><?php bloginfo('name'); ?></h1>
+    <p><?php bloginfo('description'); ?></p>
+
+    <?php
+    if ( have_posts() ) :
+        while ( have_posts() ) : the_post();
+            the_title( '<h2>', '</h2>' );
+            the_content();
+        endwhile;
+    else :
+        echo '<p>No content found</p>';
+    endif;
+    ?>
+</body>
+</html>
